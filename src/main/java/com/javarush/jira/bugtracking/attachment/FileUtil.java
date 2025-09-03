aab0134b6f110a7194e866b9e5599764f41c60fd
Pavel Ivanov <krongras@mail.ru>
2025-09-03 19:04:52 +0300

    ActivityService add methods howLongTaskInProgress, howLongTaskInTesting. ActivityRepository add methodfindByTaskIdAndStatusCode. Activity add 3 fields. Change changelog.sql

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.naming.SizeLimitExceededException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,25 +23,14 @@ import java.util.Objects;
 public class FileUtil {
     private static final String ATTACHMENT_PATH = "./attachments/%s/";
 
-//    public static void upload(MultipartFile multipartFile, String directoryPath, String fileName) {
-//        if (multipartFile.isEmpty()) {
-//            throw new IllegalRequestDataException("Select a file to upload.");
-//        }
-//
-//        File dir = new File(directoryPath);
-//        if (dir.exists() || dir.mkdirs()) {
-//            File file = new File(directoryPath + fileName);
-//            try (OutputStream outStream = new FileOutputStream(file)) {
-//                outStream.write(multipartFile.getBytes());
-//            } catch (IOException ex) {
-//                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
-//            }
-//        }
-//    }
-
     public static void upload(MultipartFile multipartFile, String directoryPath, String fileName) {
         if (multipartFile.isEmpty()) {
             throw new IllegalRequestDataException("Select a file to upload.");
+        }
+
+        long maxSize = 10 * 1024 * 1024;
+        if(multipartFile.getSize() > maxSize) {
+            throw new RuntimeException("The upload file size has ben exceeded");
         }
 
         Path dir = Paths.get(directoryPath);
